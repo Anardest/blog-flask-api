45d16cf5bbc9388bf4afbfffec207d377cc89f16
Anardest <associalgamer@gmail.com>
2025-02-13 12:23:10 +0300

    feat: Добавлено переключение отображения формы: "Создать пост"

--- main/static/js/switch_addpost.js
+++ main/static/js/switch_addpost.js
@@ -1,2 +1,14 @@
-document.getElementById('')
-// TODO: Доделать
+$(document).ready(function () {
+    $('#hideFormAddPost').hide();
+    $("#showFormAddPost").click(function () {
+        $("#addPostCard").fadeIn();  // Показываем форму
+        $("#showFormAddPost").hide();  // Скрываем кнопку "Создать пост"
+        $("#hideFormAddPost").show();  // Показываем кнопку "Отмена"
+    });
+
+    $("#hideFormAddPost").click(function () {
+        $("#addPostCard").fadeOut();  // Скрываем форму
+        $("#hideFormAddPost").hide();  // Скрываем кнопку "Отмена"
+        $("#showFormAddPost").show();  // Показываем кнопку "Создать пост"
+    });
+});
